2b00cc1916cdc0d9410c68873ed1df821c8898fd
Xavier Baque <xavier.18.2018@hotmail.com>
2023-05-02 22:06:34 -0500

    XavierRepository

---
+++ app/app.ts
@@ -0,0 +1,11 @@
+//Operador logico bitwice
+let i:number= 2;
+console.log("~i=" + ~i)
+document.write("~i=" + ~i)
+
+//Operador No Logico !
+let e: boolean = true;
+console.log("!e = " + !e);
+document.write("!e S= " + !e);
+
+
---
+++ js/app.js
@@ -0,0 +1,9 @@
+"use strict";
+//Operador logico bitwice
+let i = 2;
+console.log("~i=" + ~i);
+document.write("~i=" + ~i);
+//Operador No Logico !
+let e = true;
+console.log("!e = " + !e);
+document.write("!e S= " + !e);
